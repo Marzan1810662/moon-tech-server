ca7d9f7e23fa0a7c6bddee11bc81f3458082ef48
Marzan1810662 <hassanmarzan17@gmail.com>
2022-12-10 15:33:34 +0600

    atlas uri

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 /* mongoDB ATLAS */
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vr3qdga.mongodb.net/?retryWrites=true&w=majority`;
+ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vr3qdga.mongodb.net/?retryWrites=true&w=majority`;
 
 /* mongoDB COMPASS */
-const uri = 'mongodb://localhost:27017'
+// const uri = 'mongodb://localhost:27017'
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
